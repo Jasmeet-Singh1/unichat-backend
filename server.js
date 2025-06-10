e5ec87d388c9710f9a8caf3bc67c5986d081f9e7
Jasmeet <jassoffline586@gmail.com>
2025-06-09 17:57:47 -0700

    Initialize General Code

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+//const connectDB = require('./config/db');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Connect Database
+//connectDB();
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+
+app.get('/', (req, res) => res.json({ msg: 'Welcome to UniChat API...' }));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
