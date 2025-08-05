a704540bcc0ae81dde6f6ac03379142f58e360ee
HarleeKaur-101 <harleen.r.1805@gmail.com>
2025-08-04 17:50:12 -0700

    some changes in updateProfile ( checkpoint)

--- routes/updateProfile.js
+++ routes/updateProfile.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const updateUserProfile = require('../controllers/updateProfileController');
-const protect = require('../middleware/auth'); 
+// Import both controller functions
+const { getUserProfile, updateUserProfile } = require('../controllers/updateProfileController');
 
+const protect = require('../middleware/auth');
 
+// GET request to fetch profile
+router.get('/profile', protect, getUserProfile);
+
+// PUT request to update profile
 router.put('/profile', protect, updateUserProfile);
 
 module.exports = router;
